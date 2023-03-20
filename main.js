252e2b9b0f45ff397dcd62583ebd37cf09260e62
Andrei Nascimento <andrei.lima.nas@outlook.com>
2023-03-20 16:12:09 -0300

    Adição de comentários no código

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://realtime-database-5bfe7-default-rtdb.firebaseio.com/"
@@ -13,6 +13,7 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
+// Adiciona o valor digitado no banco de dados e limpa o input
 addButtonEl.addEventListener("click", function() {
   let inputValue = inputFieldEl.value
     
@@ -22,12 +23,21 @@ addButtonEl.addEventListener("click", function() {
 })
 
 onValue(shoppingListInDB, function(snapshot) {
-  let itemsArray = Object.values(snapshot.val())
-    
+  // Armazena as keys e values dos itens na variável
+  let itemsArray = Object.entries(snapshot.val())
+  
   clearShoppingListEl()
-    
+  
+  // Lista todos os itens adicionados
   for (let i = 0; i < itemsArray.length; i++) {
-    appendItemToShoppingListEl(itemsArray[i])
+    let currentItem = itemsArray[i]
+    
+    // Armazena a key/id do item
+    let currentItemID = currentItem[0]
+    // Armazena o value do item
+    let currentItemValue = currentItem[1]
+
+    appendItemToShoppingListEl(currentItem)
   }
 })
 
@@ -39,6 +49,23 @@ function clearInputFieldEl() {
   inputFieldEl.value = ""
 }
 
-function appendItemToShoppingListEl(itemValue) {
-  shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+function appendItemToShoppingListEl(item) {
+  let itemID = item[0]
+  let itemValue = item[1]
+  
+  let newEl = document.createElement("li")
+  
+  // Inclui o value do item para o newEl
+  newEl.textContent = itemValue
+
+  // Remove o item da tela e do DB pelo id com um double click
+  newEl.addEventListener("dblclick", function() {
+    
+    // Seleciona qual banco de dados usar e seu id
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+    remove(exactLocationOfItemInDB)
+  })
+  
+  // Adiciona o novo item na lista de compras
+  shoppingListEl.append(newEl)
 }
